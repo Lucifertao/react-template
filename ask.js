2eddddc49bed7968cea21ac6fd5cac93195fd1af
try_yang <try_yang@163.com>
2019-11-27 14:53:34 +0800

    feat: edit ask.js

---
+++ ask.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    name: 'private',
+    type: 'confirm',
+    message: 'the project is private',
+  },
+  {
+    name: 'author', // 获取选择后的结果
+    type: 'input',
+    message: 'place input author',
+  },
+  {
+    name: 'description', // 获取选择后的结果
+    type: 'input',
+    message: 'place input description',
+  },
+  {
+    name: 'license', // 获取选择后的结果
+    type: 'input',
+    message: 'license?',
+  },
+];
